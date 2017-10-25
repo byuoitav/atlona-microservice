436a30bdbe93b024d0dd8b658999ac7f06a4cd5a
jpw547 <jwerner547@gmail.com>
2017-10-25 12:45:25 -0600

    Add functions to change the input and output from a 0 based number to a 1 based number

--- handlers/standard.go
+++ handlers/standard.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	help "github.com/byuoitav/atlona-microservice/helpers"
 	"github.com/byuoitav/av-api/statusevaluators"
@@ -12,8 +13,8 @@ import (
 func SwitchInput(context echo.Context) error {
 	log.Printf("Changing inputs")
 	//Set parameters
-	input := context.Param("input")
-	output := context.Param("output")
+	input := IncPort(context.Param("input"))
+	output := IncPort(context.Param("output"))
 	address := context.Param("address")
 	//Print out the command
 	log.Printf("Routing %v to %v on %v", input, output, address)
@@ -25,12 +26,14 @@ func SwitchInput(context echo.Context) error {
 	}
 
 	log.Printf("Success")
+	input = DecPort(input)
+	output = DecPort(output)
 	return context.JSON(http.StatusOK, statusevaluators.Input{Input: input})
 }
 
 func CheckInput(context echo.Context) error {
 	//Set params, only need output to verify which input is connected.
-	output := context.Param("output")
+	output := IncPort(context.Param("output"))
 	address := context.Param("address")
 	log.Printf("Verifiying which input is connected to %v", output)
 	//Call help.GetInput func, it will return the input as a string
@@ -41,10 +44,26 @@ func CheckInput(context echo.Context) error {
 	}
 
 	log.Printf("Success")
+	resp = DecPort(resp)
 	return context.JSON(http.StatusOK, statusevaluators.Input{Input: resp})
 }
 
-// func PowerSwitch(address, command string) string {
-//   response := help.ChangePower(address, command)
-//   return response
-// }
+//Function to increment the port from a base 0 to a base 1 input/output
+func IncPort(IOport string) string {
+	x, err := strconv.Atoi(IOport)
+	if err != nil {
+		return ""
+	}
+	x++
+	return strconv.Itoa(x)
+}
+
+//Function to decrement the port from a base 1 to a base 0 input/output
+func DecPort(IOport string) string {
+	x, err := strconv.Atoi(IOport)
+	if err != nil {
+		return ""
+	}
+	x--
+	return strconv.Itoa(x)
+}
